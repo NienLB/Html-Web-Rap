9d9a12072023f711d50651e93bcdfdde73b6c516
Niên Lương <tatsuyanin@gmail.com>
2016-06-12 12:45:33 +0700

    Fix logo - Add Category

--- js/web.js
+++ js/web.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
   $('.slick-js').slick({
     dots: true,
-    arrows: false,
-    speed: 1000,
-    autoplay: true
     
+    speed: 1000,
+    autoplay: true,
+  
   });
+	$('.center-js').slick({
+	autoplay: true,
+	speed: 1000,
+   infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  centerPadding: '20px'
+});
+	
+
+
 });
